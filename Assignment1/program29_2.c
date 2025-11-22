f3b1039baf75bbe9e84eaa2ff2a949e748ec0876
Tejaswini Gunjal <tejaswinigunjal123@gmail.com>
2025-11-22 23:30:38 +0530

    Add files via upload

---
+++ Assignment1/program29_2.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+int i = 0;
+int j = 0;
+
+void Pattern(int iRow, int iCol)
+{
+     
+    for (i = 1; i <= iRow; i++)
+    {
+        for (j = 1; j <= iCol; j++)
+        {
+          if (i % 2 != 0)
+          {
+            printf("%d\t",j*2);
+          
+          }
+          else
+          {
+            printf("%d\t",j*2-1);
+          }
+          
+           
+        }
+        printf("\n");
+        
+    }
+    
+}
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows and columns\n");
+    scanf("%d %d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
