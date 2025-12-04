af00813c63939d18bf29f0188e2eb32bfefaeff3
Tejaswini Gunjal <tejaswinigunjal123@gmail.com>
2025-12-04 23:27:15 +0530

    Add files via upload

---
+++ Assignment1/program2_4.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+void Display(int iNo,int iFrequency)
+{
+    int iCnt = 0;
+
+    for(iCnt = 0; iCnt < iFrequency; iCnt++)
+    {
+        printf("%d\n",iNo);
+    }
+}
+int main()
+{
+    int iValue = 0;
+    int iCount = 0;
+
+    printf("Enter number : \n");
+    scanf("%d",&iValue);
+
+    printf("Enter Frequency :\n");
+    scanf("%d",&iCount);
+
+     printf("Output is : \n");
+    Display(iValue,iCount);
+
+   
+    return 0;
+}
---
+++ Assignment1/program2_5.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+bool CheckEvenOdd(int iNo)
+{
+    if(iNo % 2 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+
+    printf("Enter number : \n");
+    scanf("%d",&iValue);
+
+    bRet = CheckEvenOdd(iValue);
+
+   if(bRet == true)
+   {
+    printf("Even\n");
+   }
+   else
+   {
+    printf("odd\n");
+   }
+
+    return 0;
+}
